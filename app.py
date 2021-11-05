5259ea974de8e1d40af56f691ffa18bea95edf6e
Jack-Keene <jack.keene1@gmail.com>
2021-11-05 16:42:57 +0000

    Postgres Plugin Update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:m4rt9r6H@localhost/autoviews'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sadvisqnhgpsei:f592e14d8a0953a894200076a9f7d646d5a7f04cd5b5c315c287515494cdb6cc@ec2-34-226-18-183.compute-1.amazonaws.com:5432/d7dl0398o6ip5r'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sadvisqnhgpsei:f592e14d8a0953a894200076a9f7d646d5a7f04cd5b5c315c287515494cdb6cc@ec2-34-226-18-183.compute-1.amazonaws.com:5432/d7dl0398o6ip5r'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.secret_key=os.urandom(24)
